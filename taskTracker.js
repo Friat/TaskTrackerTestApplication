736aa9d33f5a881edee70061d2ce30e118497e32
Friat Mehari <Fri4Adom@gmail.com>
2019-04-30 19:58:47 -0400

    Task Tracker Test Application First commit

---
+++ taskTracker.js
@@ -0,0 +1,55 @@
+ var toPopulate = [{
+    "name": "Test Task #1",
+    "date": "12/01/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #2",
+    "date": "12/02/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #3",
+    "date": "12/03/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #4",
+    "date": "12/04/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #5",
+    "date": "12/05/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #6",
+    "date": "12/06/2012",
+    "assigned": "John Doe"
+  }, {
+    "name": "Test Task #7",
+    "date": "12/07/2012",
+    "assigned": "John Doe"
+  }];
+
+  function pageLoad(toPopulate) {
+    toPopulate.forEach(function(task) {
+      var taskItem = "<div class='task-item'><h4 class='task-name'>" + task.name + "</h4><h4 class='task-date'>" + task.date + "</h4><h4 class='task-assigned'>" + task.assigned + "</h4></div>";
+      $(".task-wrapper").prepend(taskItem);
+    });
+  };
+
+function addTask(){
+  var name = $(".name").val();
+  var date = $(".date").val();
+  var assigned = $(".assigned").val();
+  var taskItem = "<div class='task-item'><h4 class='task-name'>" + name + "</h4><h4 class='task-date'>" + date + "</h4><h4 class='task-assigned'>" + assigned + "</h4></div>";
+  $(".task-wrapper").prepend(taskItem); 
+};
+
+$(document).ready(function() {
+ 
+  pageLoad(toPopulate);
+  
+  $("#task").on('submit',function(event){
+   event.preventDefault();
+        addTask();
+  });
+
+});
